8de2950a0afbb8f5e97484077142583eae1db34d
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-12-18 13:22:44 -0700

    Added images to arrays, temp carousel in body.

--- src/Body.tsx
+++ src/Body.tsx
@@ -1,10 +1,13 @@
 import "./Body.css";
+import Carousel from "./Carousel";
+import { facilitiesImageArray } from "./ImagesArrays";
 
 export default function Body() {
   return (
     <div className="body-container">
       <div className="body-content">
         <h2 id="home">Welcome to School!</h2>
+        <Carousel images={facilitiesImageArray} />
       </div>
     </div>
   );
---
+++ src/ImagesArrays.tsx
@@ -0,0 +1,65 @@
+import artCenter from "./assets/facilities/art-center.jpg";
+import artCenter2 from "./assets/facilities/art-center2.jpg";
+import bear from "./assets/facilities/bear.jpg";
+import canopy from "./assets/facilities/canopy.jpg";
+import dramaticPlay from "./assets/facilities/dramatic-play.jpg";
+import entryway from "./assets/facilities/entryway.jpg";
+import field from "./assets/facilities/field.jpg";
+import libraryCorner from "./assets/facilities/library-corner.jpg";
+import playhouse from "./assets/facilities/playhouse.jpg";
+import playhouse2 from "./assets/facilities/playhouse2.jpg";
+import sandbox from "./assets/facilities/sandbox.jpg";
+import toys from "./assets/facilities/toys.jpg";
+import welcomeSign from "./assets/facilities/welcome-sign.jpg";
+
+import bottles from "./assets/students/bottles.jpg";
+import building from "./assets/students/building.jpg";
+import cards from "./assets/students/cards.jpg";
+import funTogether from "./assets/students/fun-together.jpg";
+import pans from "./assets/students/pans.jpg";
+import parachute from "./assets/students/parachute.jpg";
+import picnic from "./assets/students/picnic.jpg";
+import plate from "./assets/students/plate.jpg";
+import stPlayhouse from "./assets/students/playhouse.jpg";
+import santa from "./assets/students/santa.jpg";
+import science from "./assets/students/science.jpg";
+import teamwork from "./assets/students/teamwork.jpg";
+import tower from "./assets/students/tower.jpg";
+import tray from "./assets/students/tray.jpg";
+import writing from "./assets/students/writing.jpg";
+
+const facilitiesImageArray: string[] = [
+  artCenter,
+  artCenter2,
+  bear,
+  canopy,
+  dramaticPlay,
+  entryway,
+  field,
+  libraryCorner,
+  playhouse,
+  playhouse2,
+  sandbox,
+  toys,
+  welcomeSign,
+];
+
+const studentsImageArray: string[] = [
+  bottles,
+  building,
+  cards,
+  funTogether,
+  pans,
+  parachute,
+  picnic,
+  plate,
+  stPlayhouse,
+  santa,
+  science,
+  teamwork,
+  tower,
+  tray,
+  writing,
+];
+
+export { studentsImageArray, facilitiesImageArray };
