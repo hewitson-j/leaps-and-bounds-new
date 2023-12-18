b63df5b98ecd5eda86ebabe6d8266b729a21d303
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-12-18 14:30:50 -0700

    Finished styling for carousel.

--- src/Body.tsx
+++ src/Body.tsx
@@ -1,12 +1,13 @@
 import "./Body.css";
 import Carousel from "./Carousel";
-import { facilitiesImageArray } from "./ImagesArrays";
+import { facilitiesImageArray, studentsImageArray } from "./ImagesArrays";
 
 export default function Body() {
   return (
     <div className="body-container">
       <div className="body-content">
         <h2 id="home">Welcome to School!</h2>
+        <Carousel images={studentsImageArray} />
         <Carousel images={facilitiesImageArray} />
       </div>
     </div>
--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -1,7 +1,121 @@
+import { useEffect, useState } from "react";
+import "./Carousel.css";
+
 interface CarouselProps {
   images: string[];
 }
 
 export default function Carousel({ images }: CarouselProps) {
-  return <>{images}</>;
+  const [currentImage, setCurrentImage] = useState(0);
+  const [prevImage, setPrevImage] = useState(images.length - 1);
+  const [nextImage, setNextImage] = useState(1);
+
+  const [imageDimensions, setImageDimensions] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  const calculateImageDimensions = (src: string) => {
+    const img = new Image();
+    img.src = src;
+
+    img.onload = () => {
+      const aspectRatio = img.width / img.height;
+      const maxWidth = 400;
+      const maxHeight = 400;
+
+      let width = img.width;
+      let height = img.height;
+
+      if (width > maxWidth) {
+        width = maxWidth;
+        height = width / aspectRatio;
+      }
+      if (height > maxHeight) {
+        height = maxHeight;
+        width = height * aspectRatio;
+      }
+
+      setImageDimensions({ width, height });
+    };
+  };
+
+  useEffect(() => {
+    calculateImageDimensions(images[currentImage]);
+  }, [images, currentImage]);
+
+  const handleNext = () => {
+    if (currentImage === images.length - 1) {
+      setCurrentImage(0);
+    } else {
+      setCurrentImage(currentImage + 1);
+    }
+
+    if (prevImage === images.length - 1) {
+      setPrevImage(0);
+    } else {
+      setPrevImage(prevImage + 1);
+    }
+
+    if (nextImage === images.length - 1) {
+      setNextImage(0);
+    } else {
+      setNextImage(nextImage + 1);
+    }
+
+    console.log(currentImage);
+    console.log(prevImage);
+    console.log(nextImage);
+  };
+
+  const handlePrev = () => {
+    if (currentImage === images.length - 1) {
+      setCurrentImage(0);
+    } else {
+      setCurrentImage(currentImage - 1);
+    }
+
+    if (prevImage === images.length - 1) {
+      setPrevImage(0);
+    } else {
+      setPrevImage(prevImage - 1);
+    }
+
+    if (nextImage === images.length - 1) {
+      setNextImage(0);
+    } else {
+      setNextImage(nextImage - 1);
+    }
+
+    console.log(currentImage);
+    console.log(prevImage);
+    console.log(nextImage);
+  };
+
+  return (
+    <>
+      <div className="carousel-body">
+        <div className="carousel-button-div">
+          <button onClick={handlePrev} title="Back" id="carousel-back-button">
+            ← Back
+          </button>
+        </div>
+        <div id="carousel-image-container">
+          <img
+            src={images[currentImage]}
+            style={{
+              width: imageDimensions.width,
+              height: imageDimensions.height,
+            }}
+            id="carousel-image"
+          />
+        </div>
+        <div className="carousel-button-div">
+          <button onClick={handleNext} title="Next" id="carousel-next-button">
+            Next →
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
