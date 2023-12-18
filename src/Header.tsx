691b8f8a6f33559b4a8206a9077b233c64ba2406
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-12-18 12:00:33 -0700

    Added fonts, added header.

---
+++ src/Header.tsx
@@ -0,0 +1,31 @@
+import logo from "./assets/Logo.jpg";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <>
+      <div className="header-grid">
+        <div id="header-whitespace"></div>
+        <img
+          src={logo}
+          alt="Leaps and Bounds Preschool Logo"
+          id="header-logo"
+        />
+        <div id="header-contact-info">
+          <p>
+            <b>melonro143@yahoo.com</b>
+          </p>
+          <p>
+            <b>719-439-0566</b>
+          </p>
+          <a
+            href="https://www.facebook.com/MsMelanieTrujillo"
+            title="Leaps and Bounds Preschool Facebook"
+          >
+            <b>Facebook Page</b>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
