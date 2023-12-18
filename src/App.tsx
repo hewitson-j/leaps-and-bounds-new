9c64deb094495741cfafa9eb99fbca2a8d8ac981
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-12-18 12:26:31 -0700

    Added body and background image.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Body from "./Body";
 import Header from "./Header";
 
 export default function App() {
   return (
     <>
       <Header />
+      <Body />
     </>
   );
 }
---
+++ src/Body.tsx
@@ -0,0 +1,9 @@
+import "./Body.css";
+
+export default function Body() {
+  return (
+    <div className="body-container">
+      <div className="body-content">This is a test</div>
+    </div>
+  );
+}
