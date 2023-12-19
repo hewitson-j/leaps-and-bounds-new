e0eb3ed5619463d324431eb41b60b76cfea29722
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-12-18 17:04:56 -0700

    Modularized MeetTeacher.

--- src/Body.tsx
+++ src/Body.tsx
@@ -2,6 +2,7 @@ import "./Body.css";
 import Carousel from "./Carousel";
 import Contact from "./Contact";
 import { facilitiesImageArray, studentsImageArray } from "./ImagesArrays";
+import MeetTeacher from "./MeetTeacher";
 
 export default function Body() {
   return (
@@ -46,40 +47,20 @@ export default function Body() {
         </p>
         <h2>Our Facilities</h2>
         <Carousel images={facilitiesImageArray} />
-        <h2 id="meet-teacher">Meet the Teacher!</h2>
-        <p>
-          I am passionate about teaching preschool! I have been working in the
-          field of early childhood education for almost 20 years beginning in
-          high school. I am a member of NAEYC, CAEYC and am licensed with the
-          state of Colorado. After 10 years of working in the public school
-          system in Nevada I moved to Colorado Springs and began working at a
-          childcare center and did so for over two years. At that time, I
-          decided to open my own preschool classroom. I have been teaching
-          preschool-age students in my home as a licensed family childcare
-          provider for over five years. My husband and I have been married 9
-          years. We have a beautifully blended family with six children whose
-          ages range from 17- 25. We have one dog named Rosie and a cat named
-          Ophelia. I enjoy being able to teach children from my home. I love
-          working with them and seeing their faces light up when they learn
-          something new! I’m excited to have your child join our class and look
-          forward to teaching, as well as learning from them. I truly believe
-          each child has amazing potential and gifts already inside of them.
-          Thank you for considering becoming a part of the Leaps & Bounds
-          Preschool family!
-        </p>
+        <MeetTeacher />
         <h2 id="reviews">Reviews</h2>
         <p>
-          Ms. Melanie is the best and most nurturing teacher a kid could have. I
-          felt 100% comfortable sending my children to her every day. They loved
-          everything about Leaps and Bounds and so did I! We will miss Ms.
-          Melanie very much, but my kids are very ready for the next step of
-          Kindergarten thanks to her! - Tiffany C.
+          {"\t"}Ms. Melanie is the best and most nurturing teacher a kid could
+          have. I felt 100% comfortable sending my children to her every day.
+          They loved everything about Leaps and Bounds and so did I! We will
+          miss Ms. Melanie very much, but my kids are very ready for the next
+          step of Kindergarten thanks to her! - Tiffany C.
           <br />
           <br />
-          Melanie is truly amazing. Her hard work and passion for teaching shows
-          in everything she does. Tonight was my little girls graduation and it
-          makes me so sad that we won’t get to send her everyday. I knew my
-          daughter was in the best preschool as not one day went by that she
+          {"\t"}Melanie is truly amazing. Her hard work and passion for teaching
+          shows in everything she does. Tonight was my little girls graduation
+          and it makes me so sad that we won’t get to send her everyday. I knew
+          my daughter was in the best preschool as not one day went by that she
           didn’t come out with a smile on her face. The curriculum Melanie uses
           is incredible. We are going to miss Leaps and Bounds so much next year
           but I know she has had an incredible start to her learning because of
@@ -87,15 +68,15 @@ export default function Body() {
           Lucy. - Carolyne M.
           <br />
           <br />
-          My Daughter has thrived here and loves going to school. She has made
-          huge progress both academically and socially. I love that Ms. Melanie
-          does play based learning and you can tell that she cares about and
-          loves her students. The class size is small so that you child gets the
-          attention they need. She is wonderful at communicating with parents, I
-          never feel like I don't know whats going on. She always has such fun
-          and creative stations and projects for the students. I will be sending
-          my son here when he is old enough and would recommend leaps and bounds
-          to everybody. - Heather B.
+          {"\t"}My Daughter has thrived here and loves going to school. She has
+          made huge progress both academically and socially. I love that Ms.
+          Melanie does play based learning and you can tell that she cares about
+          and loves her students. The class size is small so that you child gets
+          the attention they need. She is wonderful at communicating with
+          parents, I never feel like I don't know whats going on. She always has
+          such fun and creative stations and projects for the students. I will
+          be sending my son here when he is old enough and would recommend leaps
+          and bounds to everybody. - Heather B.
           <br />
           <br />
           It’s been a pleasure watching my little girl flourish under Miss
---
+++ src/MeetTeacher.tsx
@@ -0,0 +1,32 @@
+import image from "./assets/mrs-melanie.jpg";
+import "./MeetTeacher.css";
+
+export default function MeetTeacher() {
+  return (
+    <div className="meet-teacher">
+      <h2>Meet the Teacher!</h2>
+      <img src={image} alt="Ms. Melanie" id="melanie-image" />
+      <p>
+        I am passionate about teaching preschool! I have been working in the
+        field of early childhood education for almost 20 years beginning in high
+        school. I am a member of NAEYC, CAEYC and am licensed with the state of
+        Colorado. After 10 years of working in the public school system in
+        Nevada I moved to Colorado Springs and began working at a childcare
+        center and did so for over two years. At that time, I decided to open my
+        own preschool classroom. I have been teaching preschool-age students in
+        my home as a licensed family childcare provider for over five years. My
+        husband and I have been married 9 years. We have a beautifully blended
+        family with six children whose ages range from 17- 25.
+        <br />
+        <br />
+        We have one dog named Rosie and a cat named Ophelia. I enjoy being able
+        to teach children from my home. I love working with them and seeing
+        their faces light up when they learn something new! I’m excited to have
+        your child join our class and look forward to teaching, as well as
+        learning from them. I truly believe each child has amazing potential and
+        gifts already inside of them. Thank you for considering becoming a part
+        of the Leaps & Bounds Preschool family!
+      </p>
+    </div>
+  );
+}
