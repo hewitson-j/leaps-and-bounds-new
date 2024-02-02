d6ea64912608295ea668f1a60cb04318422c06c0
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-02-02 09:59:30 -0700

    Added further links to reviews.

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -3,38 +3,54 @@ export default function Reviews() {
     <div className="reviews" id="reviews">
       <h2>Reviews</h2>
       <p className="reviews-links">
-        {"\t"}Ms. Melanie is the best and most nurturing teacher a kid could
-        have. I felt 100% comfortable sending my children to her every day. They
-        loved everything about Leaps and Bounds and so did I! We will miss Ms.
-        Melanie very much, but my kids are very ready for the next step of
-        Kindergarten thanks to her!
-        <br />
-        <i>- Tiffany C.</i>
-        <br />
-        <br />
-        {"\t"}Melanie is truly amazing. Her hard work and passion for teaching
-        shows in everything she does. Tonight was my little girls graduation and
-        it makes me so sad that we won’t get to send her everyday. I knew my
-        daughter was in the best preschool as not one day went by that she
-        didn’t come out with a smile on her face. The curriculum Melanie uses is
-        incredible. We are going to miss Leaps and Bounds so much next year but
-        I know she has had an incredible start to her learning because of you,
-        Melanie. Thank you for everything you have done this year with Lucy.
-        <br />
-        <i>- Carolyne M.</i>
+        <a
+          href="https://www.facebook.com/17802835/posts/10107192637214339/?mibextid=WC7FNe"
+          target="_blank"
+        >
+          {"\t"}Ms. Melanie is the best and most nurturing teacher a kid could
+          have. I felt 100% comfortable sending my children to her every day.
+          They loved everything about Leaps and Bounds and so did I! We will
+          miss Ms. Melanie very much, but my kids are very ready for the next
+          step of Kindergarten thanks to her!
+          <br />
+          <i>- Tiffany C.</i>
+        </a>
         <br />
         <br />
-        {"\t"}My Daughter has thrived here and loves going to school. She has
-        made huge progress both academically and socially. I love that Ms.
-        Melanie does play based learning and you can tell that she cares about
-        and loves her students. The class size is small so that you child gets
-        the attention they need. She is wonderful at communicating with parents,
-        I never feel like I don't know whats going on. She always has such fun
-        and creative stations and projects for the students. I will be sending
-        my son here when he is old enough and would recommend leaps and bounds
-        to everybody.
+        <a
+          href="https://www.facebook.com/618720275/posts/10164933455785276/?mibextid=WC7FNe"
+          target="_blank"
+        >
+          {"\t"}Melanie is truly amazing. Her hard work and passion for teaching
+          shows in everything she does. Tonight was my little girls graduation
+          and it makes me so sad that we won’t get to send her everyday. I knew
+          my daughter was in the best preschool as not one day went by that she
+          didn’t come out with a smile on her face. The curriculum Melanie uses
+          is incredible. We are going to miss Leaps and Bounds so much next year
+          but I know she has had an incredible start to her learning because of
+          you, Melanie. Thank you for everything you have done this year with
+          Lucy.
+          <br />
+          <i>- Carolyne M.</i>
+        </a>
         <br />
-        <i>- Heather B.</i>
+        <br />
+        <a
+          href="https://www.facebook.com/505403452/posts/10158513043108453/?mibextid=WC7FNe"
+          target="_blank"
+        >
+          {"\t"}My Daughter has thrived here and loves going to school. She has
+          made huge progress both academically and socially. I love that Ms.
+          Melanie does play based learning and you can tell that she cares about
+          and loves her students. The class size is small so that you child gets
+          the attention they need. She is wonderful at communicating with
+          parents, I never feel like I don't know whats going on. She always has
+          such fun and creative stations and projects for the students. I will
+          be sending my son here when he is old enough and would recommend leaps
+          and bounds to everybody.
+          <br />
+          <i>- Heather B.</i>
+        </a>
         <br />
         <br />
         <a
