6ff6bab17c01ca904b4d823a0002b4c9ee367f4a
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-02-02 10:06:10 -0700

    Added styles for reviews links.

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -4,16 +4,17 @@ export default function Reviews() {
   return (
     <div className="reviews" id="reviews">
       <h2>Reviews</h2>
-      <p className="reviews-links">
+      <p>
         <a
           href="https://www.facebook.com/17802835/posts/10107192637214339/?mibextid=WC7FNe"
           target="_blank"
+          className="reviews-links"
         >
-          {"\t"}Ms. Melanie is the best and most nurturing teacher a kid could
-          have. I felt 100% comfortable sending my children to her every day.
-          They loved everything about Leaps and Bounds and so did I! We will
-          miss Ms. Melanie very much, but my kids are very ready for the next
-          step of Kindergarten thanks to her!
+          Ms. Melanie is the best and most nurturing teacher a kid could have. I
+          felt 100% comfortable sending my children to her every day. They loved
+          everything about Leaps and Bounds and so did I! We will miss Ms.
+          Melanie very much, but my kids are very ready for the next step of
+          Kindergarten thanks to her!
           <br />
           <i>- Tiffany C.</i>
         </a>
@@ -22,11 +23,12 @@ export default function Reviews() {
         <a
           href="https://www.facebook.com/618720275/posts/10164933455785276/?mibextid=WC7FNe"
           target="_blank"
+          className="reviews-links"
         >
-          {"\t"}Melanie is truly amazing. Her hard work and passion for teaching
-          shows in everything she does. Tonight was my little girls graduation
-          and it makes me so sad that we won’t get to send her everyday. I knew
-          my daughter was in the best preschool as not one day went by that she
+          Melanie is truly amazing. Her hard work and passion for teaching shows
+          in everything she does. Tonight was my little girls graduation and it
+          makes me so sad that we won’t get to send her everyday. I knew my
+          daughter was in the best preschool as not one day went by that she
           didn’t come out with a smile on her face. The curriculum Melanie uses
           is incredible. We are going to miss Leaps and Bounds so much next year
           but I know she has had an incredible start to her learning because of
@@ -40,16 +42,17 @@ export default function Reviews() {
         <a
           href="https://www.facebook.com/505403452/posts/10158513043108453/?mibextid=WC7FNe"
           target="_blank"
+          className="reviews-links"
         >
-          {"\t"}My Daughter has thrived here and loves going to school. She has
-          made huge progress both academically and socially. I love that Ms.
-          Melanie does play based learning and you can tell that she cares about
-          and loves her students. The class size is small so that you child gets
-          the attention they need. She is wonderful at communicating with
-          parents, I never feel like I don't know whats going on. She always has
-          such fun and creative stations and projects for the students. I will
-          be sending my son here when he is old enough and would recommend leaps
-          and bounds to everybody.
+          My Daughter has thrived here and loves going to school. She has made
+          huge progress both academically and socially. I love that Ms. Melanie
+          does play based learning and you can tell that she cares about and
+          loves her students. The class size is small so that you child gets the
+          attention they need. She is wonderful at communicating with parents, I
+          never feel like I don't know whats going on. She always has such fun
+          and creative stations and projects for the students. I will be sending
+          my son here when he is old enough and would recommend leaps and bounds
+          to everybody.
           <br />
           <i>- Heather B.</i>
         </a>
@@ -58,6 +61,7 @@ export default function Reviews() {
         <a
           href="https://www.facebook.com/1381950070/posts/10214402795968689/?mibextid=WC7FNe"
           target="_blank"
+          className="reviews-links"
         >
           It’s been a pleasure watching my little girl flourish under Miss
           Melanie’s care. She is an incredible and patient teacher, who knows
