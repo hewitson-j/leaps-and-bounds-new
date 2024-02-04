578402f34aae95ee02fd83d94f2428b9ff07a618
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-02-04 01:12:37 -0700

    Removed copyright info.

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -6,8 +6,6 @@ export default function Copyright() {
       <hr />
       <p id="copyright-line">
         Leaps and Bounds Preschool is owned and operated by Melanie Trujillo.
-        <br />
-        Website Copyright © Jacob Hewitson 2023
       </p>
     </div>
   );
