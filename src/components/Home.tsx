940da4b9eefa8268f1ffb11369706b619a27b5b0
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-03-09 11:55:10 -0700

    Added aria label for main content

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ import { studentsImageArray } from "./ImagesArrays";
 
 export default function Home() {
   return (
-    <div id="home" className="home">
+    <div id="home" className="home" role="main" aria-label="Home">
       <h2>Welcome to Preschool!</h2>
       <Carousel images={studentsImageArray} />
     </div>
