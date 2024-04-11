f1b1433adc54328755d9b849e50fc25ab9465523
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-04-10 19:19:41 -0600

    Changed router to browserrouter to ensure navbar functionality

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import Router from './components/Router.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Router/>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
