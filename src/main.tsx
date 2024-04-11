3bd1d8f4c2fd914e5d485d635126a0bd440b9a85
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-04-11 08:18:09 -0600

    Added scrolltotop for each page

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import Router from './components/Router.tsx'
+import ScrollToTop from './components/ScrollToTop.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <Router/>
     </BrowserRouter>
   </React.StrictMode>,
