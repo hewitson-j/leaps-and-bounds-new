b918bec679e62eb35067b6c08dfb77d882859187
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-08-16 09:43:49 -0600

    Refactored contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,41 +1,63 @@
 import "./Contact.css";
 
+interface ContactInfoProps {
+  title: string;
+  href: string;
+  text: string;
+  id?: string;
+  role?: string;
+  ariaLabel?: string;
+}
+
+const contactInfo: ContactInfoProps[] = [
+  {
+    title: "Email",
+    href: "mailto:melonro143@yahoo.com",
+    text: "melonro143@yahoo.com",
+    id: "contact-email",
+    role: "link",
+    ariaLabel: "Open your Mailing App to send an email to melonro143@yahoo.com",
+  },
+  {
+    title: "Phone",
+    href: "tel:719-439-0566",
+    text: "719-439-0566",
+    id: "contact-phone",
+    role: "link",
+    ariaLabel: "Open your phone app to call",
+  },
+  {
+    title: "Facebook",
+    href: "https://www.facebook.com/MsMelanieTrujillo",
+    text: "Click here!",
+    id: "contact-facebook",
+    role: "link",
+    ariaLabel: "Redirect to Facebook",
+  },
+];
+
 export default function Contact() {
   return (
     <div id="contact" className="contact">
       <h2>Contact us!</h2>
       <div className="contact-body">
-        <div className="contact-child">
-          <h3>Email</h3>
-          <p>
-            <a
-              href="mailto:melonro143@yahoo.com"
-              id="contact-email"
-              role="link"
-              aria-label="Open your Mailing App to send an email to melonro143@yahoo.com"
-            >
-              melonro143@yahoo.com
-            </a>
-          </p>
-        </div>
-        <div className="contact-child">
-          <h3>Phone</h3>
-          <p>
-            <a
-              href="tel:719-439-0566"
-              role="link"
-              aria-label="Open your phone app to call"
-            >
-              719-439-0566
-            </a>
-          </p>
-        </div>
-        <div className="contact-child">
-          <h3>Facebook</h3>
-          <p>
-            <a href="https://www.facebook.com/MsMelanieTrujillo">Click here!</a>
-          </p>
-        </div>
+        {contactInfo.map((contact) => {
+          return (
+            <div className="contact-child" key={contact.title}>
+              <h3>{contact.title}</h3>
+              <p>
+                <a
+                  href={contact.href}
+                  id={contact.id}
+                  role={contact.role}
+                  aria-label={contact.ariaLabel}
+                >
+                  {contact.text}
+                </a>
+              </p>
+            </div>
+          );
+        })}
       </div>
       <a
         className="buttons"
