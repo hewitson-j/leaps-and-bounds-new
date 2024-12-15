de058144f9decb57fe71100c7240c82b3218b5e2
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2024-12-14 19:27:18 -0700

    Added banner component

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,4 +1,5 @@
 import About from "./About";
+import Banner from "./Banner";
 import "./Body.css";
 import Contact from "./Contact";
 import Footer from "./Footer";
@@ -9,6 +10,13 @@ import Reviews from "./Reviews";
 export default function Body() {
   return (
     <main className="body-container">
+      <Banner
+        dismissable
+        message="Mark your calendars! Registration for the 2025-2026 Universal Preschool school year begins at noon on Dec. 17th!"
+        link="https://upk.colorado.gov/"
+        linkName="Register Here!"
+        cookieName="hide-upk-banner"
+      />
       <div className="body-content">
         <Home />
         <About />
