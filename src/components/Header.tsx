7e1760d51eae3f5efc4883a740e665b5a3f86473
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2025-01-19 17:27:06 -0700

    Added tagline

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,14 +8,12 @@ export default function Header() {
     <header>
       <div className="header-grid">
         <div id="header-whitespace"></div>
-        <h1>
-          <img
-            src={logo}
-            alt="Leaps and Bounds Preschool Logo"
-            id="header-logo"
-            title="Leaps and Bounds Preschool Logo"
-          />
-        </h1>
+        <img
+          src={logo}
+          alt="Leaps and Bounds Preschool Logo"
+          id="header-logo"
+          title="Leaps and Bounds Preschool Logo"
+        />
         <div id="header-contact-info">
           <p>
             <b>melonro143@yahoo.com</b>
@@ -33,6 +31,9 @@ export default function Header() {
           </a>
         </div>
       </div>
+      <p id="header-tagline">
+        <i>Growing young minds by leaps & bounds.</i>
+      </p>
       <Navbar />
       <MobileNavbar />
     </header>
